c22442a18308cfcd5d5c4f8cb2dd3e667984bb2b
Kunal Sharma <kunalprompt@gmail.com>
2016-09-09 14:41:37 +0530

    passing arguments to a decorator using a decorator class

---
+++ programmingInPython/metaprogramming/decorators/argument_passing_to_decorator_using_class.py
@@ -0,0 +1,24 @@
+from functools import wraps
+
+
+class CustomLoggingDecorator(object):
+
+    def __init__(self, arg):
+        print("Custom Logging Decorator")
+        self.arg = arg
+
+    def __call__(self, func):
+
+        @wraps(func)  # this is how we can stay on the wrapped object
+        def wrapper(*args, **kwargs):
+            return func(*args)
+
+        return wrapper
+
+
+@CustomLoggingDecorator("Hello World")
+def fxn(*args):
+    return sum(args)
+
+f = fxn(1, 2, 3, 4)
+print(f, fxn.__name__)
