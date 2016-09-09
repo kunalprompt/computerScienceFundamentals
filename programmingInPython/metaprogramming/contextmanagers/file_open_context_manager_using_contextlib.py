c78dd6da6a0aa5323e94d10e3edce8598f0113b6
Kunal Sharma <kunalprompt@gmail.com>
2016-09-09 16:41:16 +0530

    contextlib creating context manager

---
+++ programmingInPython/metaprogramming/contextmanagers/file_open_context_manager_using_contextlib.py
@@ -0,0 +1,16 @@
+from contextlib import contextmanager
+
+
+@contextmanager
+def file_ctxt_mgr(filename, mode):
+    try:
+        # everything before yield is in __enter__ method
+        f = open(filename, mode)
+        yield f
+        # everything after yield is in __exit__ method
+    finally:
+        f.close()
+
+
+with file_ctxt_mgr("file_open_context_manager_using_contextlib.py", "a") as f:
+    f.write("\n# a new comment added\n")
